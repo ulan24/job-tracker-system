8cce147ed0adce118d5ed1d8de05b40bca5ac3f0
ulan24 <ulansatybaldiev6@gmail.com>
2026-06-02 23:39:26 +0600

    first steps

---
+++ src/main/java/com/jobtracker/JobTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerSystemApplication.class, args);
+	}
+
+}
