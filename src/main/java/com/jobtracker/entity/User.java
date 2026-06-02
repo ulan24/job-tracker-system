5ac13ac4c22799cb4a64edf2230ec100101609e2
ulan24 <ulansatybaldiev6@gmail.com>
2026-06-03 01:20:46 +0600

    User entity created

---
+++ src/main/java/com/jobtracker/entity/User.java
@@ -0,0 +1,27 @@
+package com.jobtracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    
+    private String role; // USER, ADMIN
+
+}
